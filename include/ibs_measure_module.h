d26276554ce77ede1fee96af1e35f263a590e2b9
newkek <gallardo.kevin0@gmail.com>
2014-04-22 15:56:16 +0200

    Another test where it doesn't work

--- include/ibs_measure_module.h
+++ include/ibs_measure_module.h
@@ -1,6 +1,12 @@
 #ifndef _H_IBS_MEASURE_MODULE_
 #define _H_IBS_MEASURE_MODULE_
 
+#define IBS_OP_LOW_VALID_BIT           (1ULL<<18)      /* bit 18 */
+#define IBS_OP_LOW_ENABLE              (1ULL<<17)      /* bit 17 */
+#define APIC_EILVT_LVTOFF_IBS          1
+#define APIC_EILVT0                    0x500
+
+
 int thread_fn(void* args);
 
 #endif /* _H_IBS_MEASURE_MODULE_ */
